7448ca82d1a64972ffa647d30b907293fd97745f
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2025-05-14 10:56:10 -0400

    Init commit

---
+++ Install-M365-Modules.ps1
@@ -0,0 +1,120 @@
+<# 
+.SYNOPSIS 
+Allows you to install the Microsoft Cloud PowerShell Modules from PSGallery.
+
+.DESCRIPTION 
+This script allows you to install the Microsoft Cloud PowerShell Modules from PSGallery.
+It will prompt you to install the required NuGet provider if it is not already installed.
+
+.PARAMETER InputPath 
+N/A.
+
+.PARAMETER OutputPath 
+N/A.
+
+.INPUTS None. 
+
+
+.OUTPUTS None. 
+
+.EXAMPLE PS> .\Update-Month.ps1 
+
+#>
+
+
+#####
+## Script is currently in development and is not ready for production use.
+####
+
+# TODO
+# Update the script to use the latest version of the modules 
+
+# TODO 
+# Add error handling to the script
+
+# TODO
+# Add logging to the script
+
+# TODO
+# Add a progress bar to the script (optional)
+
+# TODO
+# Remove deprecated modules from the script
+
+
+
+#Function for the menu
+Function Show-Menu {
+    param (
+        [string]$Title = 'MS Cloud Toolbox'
+    )
+
+    #$Selection -eq "null"
+    Write-Output "================ $Title ================"
+    Write-Output "Available Tools: Microsoft Graph, AzureAD, MSOnline, Azure, Exchange Online (V2)"
+    Write-Output "0: Press '0' to Install Microsoft Graph"
+    Write-Output "1: Press '1' to Install AzureAD"
+    Write-Output "2: Press '2' to Install MSOnline"
+    Write-Output "3: Press '3' to Install Azure PowerShell"
+    Write-Output "4: Press '4' to Install Exchange Online (V2)"
+    Write-Output "5: Press '5' to Install All Modules"
+    Write-Output "Q: Press 'Q' to quit."
+}
+
+
+#Installing Required Repos for Module Installation and setting PS Gallery as Trusted
+Write-Warning -Message 'This tool will help you install the Microsoft Cloud PowerShell Modules from PSGallery' 
+Write-Warning -Message "In order to use this tool NuGet must be installed. Continue?" -warningaction Inquire
+Install-PackageProvider -name Nuget -minimumversion 2.8.5.201 -force
+Set-PSRepository "PSGallery" -InstallationPolicy Trusted
+
+
+$AllModules = {
+$Modules = "Microsoft.Graph","AzureAD","MSOnline","Az","ExchangeOnlineManagement" 
+
+foreach ($Module in $Modules) {
+
+    Write-Verbose "Installing $Module...This may take a few minutes" -Verbose
+    Install-Module -Name $Module -Force -Verbose
+}
+}
+
+
+#Do/Until Loop for Installs and Menu Selection
+    do
+ {
+    Clear-Host
+    Show-Menu
+    $Selection = Read-Host "Please Make a Selection"
+    switch ($Selection)
+    {
+
+    '0' {
+            Install-Module -Name Microsoft.Graph -Force -Verbose
+           
+        }
+    '1' {
+            Install-Module -Name AzureAD -Force -Verbose
+            
+        }       
+    '2' {
+            Install-Module -Name MSOnline -Force -Verbose
+       
+        }
+    '3' {
+            Install-Module -Name Az -Force -Verbose
+
+        }
+    '4' {
+            Install-Module -Name ExchangeOnlineManagement -Force
+
+        }
+    '5' {
+           Invoke-Command -ScriptBlock $AllModules
+
+        }
+    }
+
+ 
+ }
+ Until ($Selection -eq 'q')
